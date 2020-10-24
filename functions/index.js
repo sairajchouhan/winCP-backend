704ba6f4ca657841b82e569eee2092f4e223fe0b
sairaj <sairaj2119@gmail.com>
2020-10-24 12:41:26 +0530

    added cors and configuration for cors

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,34 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const firebase = require('firebase');
 const express = require('express');
+const cors = require('cors');
+
+// ********************** MIDDLEWARES AND CONFIGS **********************
 const app = express();
 app.set(express.json());
+const whitelist = ['http://localhost:3000'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      // eslint-disable-next-line callback-return
+      callback(null, true);
+    } else {
+      // eslint-disable-next-line callback-return
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+app.use(cors(corsOptions));
+// ********************** MIDDLEWARES AND CONFIGS--END ******************
 
 // ********************** FILE IMPORTS **********************
-
 const { getAllWins, postOneWin } = require('./handlers/wins');
 const { signup, login } = require('./handlers/users');
 const { auth } = require('./middlewares/auth');
-
-// ********************** FILE IMPORTS--END **********************
+// ********************** FILE IMPORTS--END ******************
 
 app.get('/wins', auth, getAllWins);
 app.post('/wins', auth, postOneWin);
 app.post('/signup', signup);
 app.post('/login', login);
-
-app.get('/adsf', (req, res) => {
-  res.send('asdf');
-});
 
 exports.api = functions.https.onRequest(app);
